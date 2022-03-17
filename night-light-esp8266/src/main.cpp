0e384a44492714e4055d74aef5952e5ba3fab587
applesama <76955501+applesama@users.noreply.github.com>
2022-03-17 10:27:29 +0800

    Update main.cpp
    Mostly finished, adjustable brightness has been added, bugs fixed

--- night-light-esp8266/src/main.cpp
+++ night-light-esp8266/src/main.cpp
@@ -6,13 +6,13 @@
 
 #define MQTT_VERSION MQTT_VERSION_3_1_1
 
-const char* ap_ssid = "nightlight";
-const char* ap_password = "";//No password
+const PROGMEM char *ap_ssid = "nightlight";
+const PROGMEM char *ap_password = ""; // No password
 
 char sta_ssid[32] = {0};
 char sta_password[64] = {0};
 
-const char* webpage_html = "\
+const PROGMEM char *webpage_html = "\
 <!DOCTYPE html>\r\n\
 <html lang='en'>\r\n\
 <head>\r\n\
@@ -31,96 +31,154 @@ const char* webpage_html = "\
 </html>\r\n\
 ";
 
-IPAddress local_IP(192,168,0,1);
-IPAddress gateway(192,168,0,1);
-IPAddress subnet(255,255,255,0);
+IPAddress local_IP(192, 168, 0, 1);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 void initApConfig();
 void initWebServer();
 void connectToWifi();
 void handleRootPost();
 void handleRoot();
-void handleNotFound();
+void handleNotFound(); // web part
 
 void readWifiInfo();
-void saveWifiInfo(byte wifiWork, char sta_ssid[32], char sta_password[64]);
+void saveWifiInfo(uint8_t wifiWork, char sta_ssid[32], char sta_password[64]);
+void EEPROMUpdate(int const address, uint8_t const value); // wifi part
+const uint8_t EEPROM_SIZE = 128;
 
-void EEPROMUpdate(int const address, uint8_t const value);
+void publishLightState();
+void publishLightBrightness();
+void callback(char *p_topic, byte *p_payload, unsigned int p_length);
+void connectToMQTT();
+void setBrightness();
+unsigned long reconnectTime = 0;
 
+const PROGMEM char *MQTT_CLIENT_ID = "bedroom_night_light";
+const PROGMEM char *MQTT_SERVER_IP = "192.168.31.128";
+const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
+const PROGMEM char *MQTT_USER = "UserOfMQTT";
+const PROGMEM char *MQTT_PASSWORD = "mqtt1234";
+
+// MQTT: topics
+const PROGMEM char *MQTT_LIGHT_STATE_TOPIC = "bedroom/light/status";
+const PROGMEM char *MQTT_LIGHT_COMMAND_TOPIC = "bedroom/light/switch";
+// state
+const PROGMEM char *MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC = "bedroom/brightness/status";
+const PROGMEM char *MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC = "bedroom/brightness/set";
+// brightness
+
+// sleep mode
+// not finied yet
+
+boolean light_state = false;
+uint8_t light_brightness = 100;
+// variables used to store the state and the brightness
+const uint8_t MSG_BUFFER_SIZE = 20;
+char msg_buffer[MSG_BUFFER_SIZE];
+// buffer used to send/receive data with MQTT
+const PROGMEM char *LIGHT_ON = "ON";
+const PROGMEM char *LIGHT_OFF = "OFF";
+// payloads by default (on/off)
+#define LIGHT_PIN 0
+
+// light pin
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 ESP8266WebServer server(80);
 
-void setup(void) {
+void setup(void)
+{
+  pinMode(LIGHT_PIN, OUTPUT);
   Serial.begin(9600);
-  EEPROM.begin(128);
-  for(int i = 0; i < 25; i++){
-    Serial.print(EEPROM.read(i));
-    Serial.print(" ");
-  }
+  EEPROM.begin(EEPROM_SIZE);
   Serial.println();
 
-  if(EEPROM.read(0) != 1){
+  if (EEPROM.read(0) != 1)
+  {
     initApConfig();
     initWebServer();
     Serial.printf("Open http://%s in your browser\n", WiFi.softAPIP().toString().c_str());
-  }else{
+  }
+  else
+  {
     readWifiInfo();
     connectToWifi();
   }
-  
-  
 
-  
+  pinMode(LIGHT_PIN, OUTPUT);
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setCallback(callback);
 }
 
-void loop(void) {
+void loop(void)
+{
   server.handleClient();
+  if (!client.connected())
+  {
+    connectToMQTT();
+  }
+  client.loop();
 }
 
-void initApConfig(){
+void initApConfig()
+{
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(local_IP, gateway, subnet);
   WiFi.softAP(ap_ssid, ap_password);
 }
 
-void initWebServer(){
+void initWebServer()
+{
   server.on("/", HTTP_GET, handleRoot);
   server.on("/", HTTP_POST, handleRootPost);
   server.onNotFound(handleNotFound);
 
-  server.begin();   
+  server.begin();
 }
 
-void connectToWifi(){
+void connectToWifi()
+{
   WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.begin(sta_ssid, sta_password);
 
   int waittingTime = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-          delay(500);
-          waittingTime++;
-          Serial.print(".");
-          if(waittingTime >= 40){
-            waittingTime = 0;
-            EEPROMUpdate(0, 0);//set wifi as not available
-            EEPROM.commit();
-            ESP.restart();
-          }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    waittingTime++;
+    Serial.print(".");
+    if (waittingTime >= 40)
+    {
+      waittingTime = 0;
+      EEPROMUpdate(0, 0); // set wifi as not available
+      EEPROM.commit();
+      ESP.restart();
+    }
   }
 }
 
-void handleRootPost() {
-  if (server.hasArg("ssid")) {
+void handleRootPost()
+{
+  if (server.hasArg("ssid"))
+  {
     strcpy(sta_ssid, server.arg("ssid").c_str());
-  } else {
+  }
+  else
+  {
     server.send(200, "text/html", "<meta charset='UTF-8'>error, ssid not found ");
     return;
   }
 
-  if (server.hasArg("password")) {
+  if (server.hasArg("password"))
+  {
     strcpy(sta_password, server.arg("password").c_str());
-  } else {
+  }
+  else
+  {
     server.send(200, "text/html", "<meta charset='UTF-8'>error, Password not found ");
     return;
   }
@@ -133,65 +191,179 @@ void handleRootPost() {
   connectToWifi();
 }
 
-void handleRoot() {
+void handleRoot()
+{
   server.send(200, "text/html", webpage_html);
 }
 
-void handleNotFound() {
+void handleNotFound()
+{
   String message = "File Not Found\n\n";
   server.send(404, "text/plain", message);
 }
 
-void saveWifiInfo(byte wifiWork, char sta_ssid[32], char sta_password[64]){
+void saveWifiInfo(uint8_t wifiWork, char sta_ssid[32], char sta_password[64])
+{
   EEPROMUpdate(0, wifiWork);
 
-  EEPROMUpdate(1, (byte)strlen(sta_ssid));//write the length of ssid
+  EEPROMUpdate(1, (byte)strlen(sta_ssid)); // write the length of ssid
 
   byte index = 2;
-  for(int i = 0; i < (int)strlen(sta_ssid); i ++){
+  for (int i = 0; i < (int)strlen(sta_ssid); i++)
+  {
     EEPROMUpdate(index, sta_ssid[i]);
-    index ++;
-  }//write ssid
+    index++;
+  } // write ssid
 
-  EEPROMUpdate(index, (byte)strlen(sta_password));//write the length of password
-  index ++;
+  EEPROMUpdate(index, (byte)strlen(sta_password)); // write the length of password
+  index++;
 
-  for(int i = 0; i < (int)strlen(sta_password); i ++){
+  for (int i = 0; i < (int)strlen(sta_password); i++)
+  {
     EEPROMUpdate(index, sta_password[i]);
-    index ++;
-  }//write password
+    index++;
+  } // write password
 
   EEPROM.commit();
 }
 
-void readWifiInfo(){
+void readWifiInfo()
+{
   byte length = EEPROM.read(1);
 
   byte index1 = 2;
   while (index1 < length + 2)
   {
     sta_ssid[index1 - 2] = EEPROM.read(index1);
-    index1 ++;
+    index1++;
   }
   length = EEPROM.read(index1);
 
-  index1 ++;
+  index1++;
   byte index2 = 0;
-  while (index2 < length){
+  while (index2 < length)
+  {
     sta_password[index2] = EEPROM.read(index1);
-    index2 ++;
-    index1 ++;
+    index2++;
+    index1++;
   }
-  
-  
 }
 
-void EEPROMUpdate(int const address, uint8_t const value){
+void EEPROMUpdate(int const address, uint8_t const value)
+{
   /*
   It seems like EEPROM.h in ESP8266 is a decrepit version, does not like the same library's in arduino
   This function is used to replace the update() in arduino's EEPROM.h
   */
-  if(EEPROM.read(address) != value){
+  if (EEPROM.read(address) != value)
+  {
     EEPROM.write(address, value);
+  }
+}
+
+void publishLightState()
+{
+  if (light_state)
+  { // function called to publish the state of the led (on/off)
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_ON, true);
+    digitalWrite(LIGHT_PIN, 1);
+  }
+  else
+  {
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_OFF, true);
+    digitalWrite(LIGHT_PIN, 0);
+  }
+}
+
+void publishLightBrightness()
+{
+  // function called to publish the brightness of the led (0-100)
+  snprintf(msg_buffer, MSG_BUFFER_SIZE, "%d", light_brightness);
+  client.publish(MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC, msg_buffer, true);
+}
+
+void connectToMQTT()
+{
+  // Loop until connection lost
+  while (!client.connected())
+  {
+    Serial.println("connect to MQTT server");
+    // try to connect
+    if (millis() - reconnectTime < 0)
+    { // if internal timer(millis) leaked and reset after 50 days
+      reconnectTime = 0;
+    }
+    if (millis() - reconnectTime > 3000)
+    {
+      if (client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD))
+      {
+        Serial.println("connected");
+
+        // Once connected, publish an announcement...
+        // publish the initial values
+        publishLightState();
+        publishLightBrightness();
+
+        // ... and resubscribe
+        client.subscribe(MQTT_LIGHT_COMMAND_TOPIC);
+        client.subscribe(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC);
+      }
+      else
+      {
+        Serial.print("Error:");
+        Serial.println(client.state());
+        Serial.println("reconnect in 3 seconds");
+        reconnectTime = millis();
+      }
+    }
+  }
+}
+
+void callback(char *p_topic, byte *p_payload, unsigned int p_length)
+{
+  // concat the payload into a string
+  String payload;
+  for (uint8_t i = 0; i < p_length; i++)
+  {
+    payload.concat((char)p_payload[i]);
+  }
+  // handle message topic
+  if (String(MQTT_LIGHT_COMMAND_TOPIC).equals(p_topic))
+  {
+    // open the light or turn it off
+    if (payload.equals(String(LIGHT_ON)))
+    {
+      if (light_state != true)
+      {
+        light_state = true;
+        publishLightState();
+      }
+    }
+    else if (payload.equals(String(LIGHT_OFF)))
+    {
+      if (light_state != false)
+      {
+        light_state = false;
+        publishLightState();
+      }
+    }
+  }
+  else if (String(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC).equals(p_topic))
+  {
+    uint8_t brightness = payload.toInt();
+    if (brightness > 0 && brightness < 100)
+    {
+      light_brightness = brightness;
+      setBrightness();
+      publishLightBrightness();
+    }
+  }
+}
+
+void setBrightness()
+{
+  if (light_state)
+  {
+    analogWrite(LIGHT_PIN, map(light_brightness, 0, 100, 0, 255));
   }
 }
